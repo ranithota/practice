45ead48d3cf6ed920eb9131e9129e6d09f5ff2e5
Rani <mahithadandu25@gmail.com>
2018-08-12 18:47:01 +0530

    Formated java file

--- src/com/ts/SampleServlet.java
+++ src/com/ts/SampleServlet.java
@@ -9,32 +9,30 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 //@WebServlet("/SampleServlet")
 public class SampleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-	String a,b;
-	public void init(){
-		a=getInitParameter("org");
-		b=getInitParameter("com");
-	
-		
+
+	String a, b;
+
+	public void init() {
+		a = getInitParameter("org");
+		b = getInitParameter("com");
+
 	}
-       
-   
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		PrintWriter out =response.getWriter();
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		PrintWriter out = response.getWriter();
 		out.print("hello");
-		
-		
+
 		out.print(a);
 		out.print(b);
 	}
 
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
 
