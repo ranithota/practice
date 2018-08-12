d45ab085a7948815d4d0caabb20faf0e272f2baf
Rani <mahithadandu25@gmail.com>
2018-08-12 18:35:34 +0530

    My first commit

---
+++ src/com/ts/SampleServlet.java
@@ -0,0 +1,41 @@
+package com.ts;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+//@WebServlet("/SampleServlet")
+public class SampleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	String a,b;
+	public void init(){
+		a=getInitParameter("org");
+		b=getInitParameter("com");
+	
+		
+	}
+       
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out =response.getWriter();
+		out.print("hello");
+		
+		
+		out.print(a);
+		out.print(b);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
